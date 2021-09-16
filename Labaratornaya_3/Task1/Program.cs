d1dedbbfe2534dceca13fcd79b7320754e9dc09f
giezz <giezz@vk.com>
2021-09-16 21:46:00 +0300

    lab3

---
+++ Labaratornaya_3/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = 0;
+            double B = 3 * Math.PI;
+            double step = Math.PI / 2;
+            double y;
+
+            for (double i = A; i <= B; i += step)
+            {
+                y = 0.5 * i + Math.Pow(Math.Sin(i), 2);
+                Console.WriteLine($"x = {i} , y = {y}");
+            }
+        }
+    }
+}
---
+++ Labaratornaya_3/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double farenheit;
+
+            for(double i = 15; i <= 30; i++)
+            {
+                farenheit = 1.8 * i + 32;
+                Console.WriteLine($"{i} градусов цельсия  = {farenheit} градусов Фаренгейта");
+            }
+        }
+    }
+}
