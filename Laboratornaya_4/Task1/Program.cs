5153224918f9ed7f39f27b4a640a554fba89a0d0
giezz <giezz@vk.com>
2021-09-16 22:15:30 +0300

    lab4 added

---
+++ Laboratornaya_4/Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер осеннего месяца");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            switch(x)
+            {
+                case 9:
+                    Console.WriteLine("Сентябрь");
+                    break;
+                case 10:
+                    Console.WriteLine("Октябрь");
+                    break;
+                case 11:
+                    Console.WriteLine("Ноябрь");
+                    break;
+                default:
+                    Console.WriteLine("Месяц не осенний");
+                    break;
+            }
+        }
+    }
+}
