c834dc8014000ed445041896bd02596cdb58f508
giezz <giezz@vk.com>
2021-09-16 23:44:36 +0300

    Lab6 Added

---
+++ Laboratornaya_6/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int negativeCount = 0;
+            int positiveCount = 0;
+            int[] arr = new int[20];
+            Random random = new Random();
+
+            for (int i = 0; i <arr.Length; i++)
+            {
+                arr[i] = random.Next(-100, 100);
+                Console.Write(arr[i] + " ");
+
+                if (arr[i] > 0) positiveCount++;
+                if (arr[i] < 0) negativeCount++;
+            }
+
+            Console.WriteLine($"\n Положительных чисел в массиве: {positiveCount} \n Отрицательных чисел в массиве: {negativeCount}");
+        }
+    }
+}
---
+++ Laboratornaya_6/Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[15];
+            Random random = new Random();
+
+            int temp = 10000;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(-100, 100);
+                Console.Write(arr[i] + " ");
+
+                if (arr[i] > 0)
+                {
+                    if (arr[i] < temp) temp = arr[i];
+                }
+            }
+
+            Console.WriteLine($"\n Наименшьий положительный элемент массива: {temp}");
+        }
+    }
+}
