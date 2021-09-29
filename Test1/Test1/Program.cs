995d8c2678e502e017c1598eab020892750cd29c
giezz <giezz@vk.com>
2021-09-30 00:37:57 +0300

    Задание ко 2 лекции

---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Test1
+{
+    class Program
+    {
+        public int[] x = new int[100000000];
+        static void Main(string[] args)
+        {
+            var stopWatchA = new Stopwatch();
+            var stopWatchB = new Stopwatch();
+            var prog = new Program();
+            
+            stopWatchA.Start();
+            prog.A();
+            stopWatchA.Stop();
+
+            stopWatchB.Start();
+            prog.B();
+            stopWatchB.Stop();
+
+            Console.WriteLine($"A \t{stopWatchA.Elapsed}\nB \t{stopWatchB.Elapsed}");
+        }
+
+        void A()
+        {
+            var y = x;
+            for (int i = 0; i < 100000000; i++)
+            {
+                y[i] = i;
+            }
+        }
+
+        void B()
+        {
+            for (int i = 0; i < 100000000; i++)
+            {
+                x[i] = i;
+            }
+        }
+    }
+}
---
+++ Test1/Test2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Test2
+{
+    class Program
+    {
+        public int[] _a = new int[10000000];
+        static void Main(string[] args)
+        {
+            var stopWatchA = new Stopwatch();
+            var stopWatchB = new Stopwatch();
+            var prog = new Program();
+
+            stopWatchA.Start();
+            prog.A();
+            stopWatchA.Stop();
+
+            stopWatchB.Start();
+            prog.B();
+            stopWatchB.Stop();
+
+            Console.WriteLine($"A \t{stopWatchA.Elapsed}\nB \t{stopWatchB.Elapsed}");
+        }
+
+        void A()
+        {
+            var a = _a;
+            var c = new int[a.Length];
+            for (int i = 0; i <= a.Length - 1; i++)
+            {
+                c[i] = a[i];
+            }
+        }
+
+        void B()
+        {
+            var a = _a;
+            var c = new int[a.Length];
+            for (int i = 0; i < a.Length; i++)
+            {
+                c[i] = a[i];
+            }
+        }
+    }
+}
